121743960bc4c353c74e6b057fe49cb55a776518
Dennis <dping@avans.nl>
2014-12-15 14:40:31 +0100

    Een simpele testbutton

--- FIND-Breda/FIND-Breda/Screen/HelpView.xaml.cs
+++ FIND-Breda/FIND-Breda/Screen/HelpView.xaml.cs
@@ -12,24 +12,19 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.UI.Xaml.Controls.Maps;
-using FIND_Breda.Screen;
 
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
-
-namespace FIND_Breda
+namespace FIND_Breda.Screen
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class HelpView : Page
     {
-        public MainPage()
+        public HelpView()
         {
             this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
         /// <summary>
@@ -39,18 +34,11 @@ namespace FIND_Breda
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Frame.Navigate(typeof(HelpView));
+
         }
     }
 }
