7d4b5f6f5fa3b6a0cb0e7297dae9f3e16b69b102
stefan-antonissen <stefan.antonissen@gmail.com>
2014-12-15 10:52:55 +0100

    Project aangemaakt

---
+++ FIND-Breda/FIND-Breda/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using FIND_Breda.Resources;
+
+namespace FIND_Breda
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
