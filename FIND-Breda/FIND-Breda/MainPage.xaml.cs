0e666748ee3b2298b5c5c474d6ac39e1c84348fd
stefan-antonissen <stefan.antonissen@gmail.com>
2014-12-15 14:15:03 +0100

    MainPage maps imported

--- FIND-Breda/FIND-Breda/MainPage.xaml.cs
+++ FIND-Breda/FIND-Breda/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls.Maps;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
